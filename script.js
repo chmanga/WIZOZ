a7297a2aa0508d1726e0ea456201b400bb7b712f
chmanga <Chimangakenge1@gmail.com>
2025-03-07 23:25:08 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Script to make the Yellow Brick Road button interactive
+document.querySelector(".yellow-road-button").addEventListener("click", function() {
+    alert("You're now traveling down the Yellow Brick Road! Follow your adventure!");
+});
